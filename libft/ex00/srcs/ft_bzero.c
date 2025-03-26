871b3728530e4c1873d0cd6de0a7582af4686918
Tanmay <tanmaypandya159@gmail.com>
2025-03-26 13:52:21 +0100

    Add some new memory functions, was not familiar with them earlier , but yea learned about them right now<srcs>

---
+++ libft/ex00/srcs/ft_bzero.c
@@ -0,0 +1,16 @@
+void	ft_bzero(void *s, size_t n)
+{
+	if (n == 0)
+		return ;
+	size_t	i;
+	unsigned char	*ptr;
+
+	i = 0;
+	ptr = (unsigned char *)s;
+	while (i < n)
+	{
+		ptr[i] = 0x00;
+		i++;
+	}
+}
+
---
+++ libft/ex00/srcs/ft_memset.c
@@ -0,0 +1,14 @@
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t	i;
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *)s;
+	i = 0;
+	while (i < len)
+	{
+		ptr[i] = (unsigned char)c;
+		i++;
+	}
+	return b;
+}
