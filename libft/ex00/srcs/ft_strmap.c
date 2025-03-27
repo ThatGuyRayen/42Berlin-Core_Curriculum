a8428aea4dadd27a6435d1fe43fb1d1a42cd982a
Tanmay <tanmaypandya159@gmail.com>
2025-03-27 10:36:50 +0100

    adding some final mandatory functions in library<srcs>

---
+++ libft/ex00/srcs/ft_strmap.c
@@ -0,0 +1,24 @@
+#include "libft.h"
+
+char	*ft_strmap(char const *s, char(*f)(char))
+{
+	int		i;
+	int		len;
+	char	*str;
+
+
+	if (!s || !f)
+		return (NULL);
+	i = 0;
+	len = ft_strlen(s);
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = f(s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
